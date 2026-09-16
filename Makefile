f1551e46e8f3d8a6501b6b886bf435bf3f373c64
= <=>
2026-09-16 18:53:25 +0300

    Changed makefile to detect the platform and fixed an unknown type error in tky_ntree.h

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
 SRCS = $(wildcard ./srcs/*.c)
 cc = gcc
+
+ifeq ($(OS),Windows_NT)
+
+target_lib = libtkyds.dll
+
+else
+
 target_lib = libtkyds.so
+
+endif
 
 build_lib:
 	$(cc) $(SRCS) -shared -fPIC -o $(target_lib)
--- headers/tky_ntree.h
+++ headers/tky_ntree.h
@@ -1,12 +1,12 @@
 #ifndef TKY_NTREE
 #define TKY_NTREE
 
-typedef struct tky_ntnode;
+typedef struct tky_ntnode tky_ntnode;
 
 tky_ntnode*		ntnode_create(void* data);
 void			ntnode_destroy(tky_ntnode* n);
 
-typedef struct tky_ntree;
+typedef struct tky_ntree tky_ntree;
 
 tky_ntree*		ntree_init();
 void			ntree_add_child(void* data);
